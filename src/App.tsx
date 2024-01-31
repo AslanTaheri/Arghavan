e57c4019f55e4058d67c3871d744a6c81b5b744f
unknown <aslan.thr@gmail.com>
2024-01-31 16:32:47 -0500

    config layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import "./App.css";
 
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// Layouts
+import RootLayout from "./Layout/RootLayout";
+
+// Pages
+import Home from "./pages/Home";
+
+const routes = createRoutesFromElements(
+  <Route path="/" element={<RootLayout />}>
+    <Route index element={<Home />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routes);
+
 function App() {
-  return (
-    <article className="container">
-      <h1>🌎 Hello, world!</h1>
-      <div></div>
-      <p>
-        Hi, I am Arghavan. I am a mission driven, data led product manager. I am
-        passionate about building teams, products, and habitable spaces that can
-        create emotional connection with users, leave positive social impact,
-        and support a sustainable future for our world.
-      </p>
-    </article>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <article className="container">
+      <h1>🌎 Hello, world!</h1>
+      <div></div>
+      <p>
+        Hi, I am Arghavan. I am a mission driven, data led product manager. I am
+        passionate about building teams, products, and habitable spaces that can
+        create emotional connection with users, leave positive social impact,
+        and support a sustainable future for our world.
+      </p>
+    </article>
+  );
+};
+
+export default Home;
